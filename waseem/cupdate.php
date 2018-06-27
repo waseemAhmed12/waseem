d2ed16bbc02ad12c4afa181483e6202116d51eee
waseemAhmed12 <40606154+waseemAhmed12@users.noreply.github.com>
2018-06-27 10:17:43 +0500

    Query
    $query = "UPDATE employee SET $FNAME='$VALUE' WHERE id='$RID'";

--- waseem/cupdate.php
+++ waseem/cupdate.php
@@ -3,7 +3,7 @@ $connect = mysqli_connect("localhost", "root", "", "testing1");
 $FNAME = $_POST['name'];
 $VALUE = $_POST['value'];
 $RID = $_POST['pk'];
-$query = "UPDATE emp_products SET $FNAME='$VALUE' WHERE id='$RID'";
+$query = "UPDATE employee SET $FNAME='$VALUE' WHERE id='$RID'";
 if (mysqli_query($connect, $query)){echo 'done';}
 else{echo 'oooops';}
 ?>
